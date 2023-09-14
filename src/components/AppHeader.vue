c2f431236d7a7acd18a3287dc7e6a28222dc24cb
IreneChicca <irene.chicca@outlook.com>
2023-09-14 14:46:34 +0200

    Setup

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+<template>
+  <h1>prova 1</h1>
+</template>
+<style></style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+<template>
+  <h1>prova 2</h1>
+</template>
+<style></style>
