6b58132c71e38d621da46f9d16c40c300b287862
IreneChicca <irene.chicca@outlook.com>
2023-09-14 15:40:11 +0200

    Add event btn

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,27 @@
 <script>
 import AppHeader from "./components/AppHeader.vue";
 import AppMain from "./components/AppMain.vue";
+import { store } from "./data/store";
 
 export default {
   data() {
-    return {};
+    return {
+      store,
+    };
   },
 
   components: { AppHeader, AppMain },
+
+  methods: {
+    searchElement() {
+      console.log("ok");
+    },
+  },
 };
 </script>
 
 <template>
-  <AppHeader />
+  <AppHeader @search-btn-click="searchElement" />
   <AppMain />
 </template>
 
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -3,9 +3,29 @@ export default {
   data() {
     return {};
   },
+
+  emits: ["search-btn-click"],
 };
 </script>
 <template>
-  <h1>prova 1</h1>
+  <div class="search w-50 container p-4">
+    <div class="input-group mb-3">
+      <input
+        type="text"
+        class="form-control"
+        placeholder="Scrivi qualcosa.."
+        aria-label="Recipient's username"
+        aria-describedby="button-addon2"
+      />
+      <button
+        class="btn btn-outline-secondary"
+        type="button"
+        id="button-addon2"
+        @click="$emit('search-btn-click')"
+      >
+        Cerca
+      </button>
+    </div>
+  </div>
 </template>
 <style></style>
